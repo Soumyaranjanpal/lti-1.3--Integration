912448fd41cdd6ab891ed20d1fbd429f8006cf89
Soumyaranjan Pal <spal@wri.wolfram.com>
2023-03-10 16:31:19 +0530

    update setup code

---
+++ server/index.js
@@ -0,0 +1,72 @@
+require('dotenv').config()
+const path = require('path')
+const routes = require('./src/routes')
+
+const lti = require('ltijs').Provider
+
+
+// Setup
+lti.setup(process.env.LTI_KEY,
+  {
+    url: 'mongodb://' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?authSource=admin', // connection to mongodb
+  }
+  // , 
+
+  //         { 
+  //           tokenMaxAge: 60 // Setting maximum token age as 60 seconds
+  //         }
+          ,{
+  staticPath: path.join(__dirname, './public'), // Path to static files
+  cookies: {
+    secure: true, // Set secure to true if the testing platform is in a different domain and https is being used
+    sameSite: 'None' // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+  },
+  devMode: true // Set DevMode to true if the testing platform is in a different domain and https is not being used
+})
+
+// When receiving successful LTI launch redirects to app
+lti.onConnect(async (token, req, res) => {
+  return res.sendFile(path.join(__dirname, './public/index.html'))
+  // return lti.redirect(res, `http://localhost:${process.env.CLIENTPORT}`);
+})
+
+// When receiving deep linking request redirects to deep screen
+lti.onDeepLinking(async (token, req, res) => {
+  return lti.redirect(res, '/deeplink', { newResource: true })
+})
+
+// Setting up routes
+lti.app.use(routes)
+
+// Setup function
+const setup = async () => {
+  await lti.deploy({ port: process.env.PORT })
+
+  /**
+   * Register platform
+   */
+  await lti.registerPlatform({
+    url: 'https://canvas.instructure.com', // or url : 'https://canvas.exampledomain.com' (depends on config form Canvas instance) if iss is changed in config/security.yml file! It must be the same as the iss
+    name: 'http://testsupport1lx:8500', // domain name from canvas instance
+    clientId: '10000000000001', // clientid from the lti plugin which you get inside canvas after installing the plugin
+    authenticationEndpoint: 'http://testsupport1lx:8500/api/lti/authorize_redirect',
+    accesstokenEndpoint: 'http://testsupport1lx:8500/login/oauth2/token',
+    authConfig: { method: 'JWK_SET', key: 'http://testsupport1lx:8500/api/lti/security/jwks' }
+  })
+
+
+
+  // For other plattforms, for example moodle:
+
+  // Moodle EXAMPLE
+  // await lti.registerPlatform({
+  //   url: 'http://localhost/moodle',
+  //   name: 'Platform',
+  //   clientId: 'CLIENTID',
+  //   authenticationEndpoint: 'http://localhost/moodle/mod/lti/auth.php',
+  //   accesstokenEndpoint: 'http://localhost/moodle/mod/lti/token.php',
+  //   authConfig: { method: 'JWK_SET', key: 'http://localhost/moodle/mod/lti/certs.php' }
+  // })
+}
+
+setup()
---
+++ server/public/precache-manifest.573876cebaad0fb91268c1500e475442.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1afe86451cd80a0e7435af7f8a1cb95",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1688303332038975db3f",
+    "url": "./static/css/2.519e0617.chunk.css"
+  },
+  {
+    "revision": "662f345dd3793df288bb",
+    "url": "./static/css/main.9727875f.chunk.css"
+  },
+  {
+    "revision": "1688303332038975db3f",
+    "url": "./static/js/2.9621677c.chunk.js"
+  },
+  {
+    "revision": "56a0eb7c93f3b1b43b686a573f3bda82",
+    "url": "./static/js/2.9621677c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "662f345dd3793df288bb",
+    "url": "./static/js/main.4b3897ec.chunk.js"
+  },
+  {
+    "revision": "b6d405b37e59947ce6c7",
+    "url": "./static/js/runtime-main.bb50f6a8.js"
+  },
+  {
+    "revision": "f88004a7c654b531da5e01390c247a4b",
+    "url": "./static/media/4.f88004a7.PNG"
+  },
+  {
+    "revision": "5e6166a3fa7b821178aa42ffca461975",
+    "url": "./static/media/6.5e6166a3.PNG"
+  },
+  {
+    "revision": "ac20c60de66b16726ce583dece1009ea",
+    "url": "./static/media/8.ac20c60d.PNG"
+  },
+  {
+    "revision": "e0896cb62a2a807e150b5a682293d775",
+    "url": "./static/media/9.e0896cb6.PNG"
+  }
+]);
